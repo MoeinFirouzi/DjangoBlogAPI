26cea624b8b7357d3040a23151b3d9e62b5112e7
MoeinFirouzi <firouzi.m.moein@gmail.com>
2023-05-16 22:58:21 +0330

    chanege AuthorRegister's url

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -10,6 +10,6 @@ app_name = "account-api-v1"
 urlpatterns = [
     path("login/", CustomObtainAuthToken.as_view(), name="user-login"),
     path("register/", UserRegister.as_view(), name="user-register"),
-    path("register-author/", AuthorRegister.as_view(), name="author-register"),
+    path("author/", AuthorRegister.as_view(), name="author-register"),
     path("author/<int:pk>/", AuthorDetail.as_view(), name="author-detail"),
 ]
