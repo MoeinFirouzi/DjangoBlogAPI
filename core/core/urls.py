b97f7939ee4c462c68ecd4af85f393a51250b728
MoeinFirouzi <firouzi.m.moein@gmail.com>
2023-05-17 00:02:41 +0330

    change openapi schema_view

--- core/core/urls.py
+++ core/core/urls.py
@@ -24,12 +24,10 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="DjangoBlogAPI API",
         default_version="v1",
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
+        contact=openapi.Contact(email="firouzi.m.moein@gmail.com"),
+        license=openapi.License(name="MIT License"),
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
