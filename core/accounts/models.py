9633b827bd6ef434d7df6938fad165a8d6e5a879
MoeinFirouzi <firouzi.m.moein@gmail.com>
2023-05-24 17:15:05 +0330

    a little change in accounts model

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -58,7 +58,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 
 class Author(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     register_time = models.DateTimeField(auto_now_add=True)
     company = models.CharField(max_length=255, null=True)
     address = models.TextField(blank=True)
